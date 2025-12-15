a93bfce28eb11c4babac38658f5ac563b601950c
jagapathi8877 <jagapathinamala@gmail.com>
2025-12-15 11:47:10 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (23.02%) - LeetSync

--- 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.cpp
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.cpp
@@ -4,11 +4,10 @@ public:
         vector<int>ans(arr.size());
         sort(arr.begin() , arr.end()) ;
         queue<int>q ;
-        int j = 0 ;
         for(int i = 0 ; i < arr.size() ; i++) q.push(i) ;
         for(int i = 0; i < arr.size() ; i++)
         {
-            ans[q.front()] = arr[j++];
+            ans[q.front()] = arr[i];
             q.pop();
             q.push(q.front()) ;
             q.pop();
