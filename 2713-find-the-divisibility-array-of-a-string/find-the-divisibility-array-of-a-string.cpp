93996a9cb0b623d6734c014063427344e0161a63
jagapathi8877 <jagapathinamala@gmail.com>
2026-01-28 22:25:41 +0530

    Time: 4 ms (82.65%) | Memory: 29.8 MB (20.96%) - LeetSync

---
+++ 2713-find-the-divisibility-array-of-a-string/find-the-divisibility-array-of-a-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> divisibilityArray(string word, int m) {
+        vector<int>ans ;
+        int n = word.size() ;
+        long long num = 0 ; 
+        for(int i = 0 ; i < n ; i++)
+        {
+            num = num*10 + (word[i] - '0') ;
+            num %= m ;
+            if(num == 0) ans.push_back(1);
+            else ans.push_back(0);
+        }
+        return ans ;
+    }
+};
