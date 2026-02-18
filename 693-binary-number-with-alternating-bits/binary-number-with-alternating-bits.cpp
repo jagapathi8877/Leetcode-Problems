d689f441bb444e8f66f12fd1fa5b237f9dd33a81
Jagapathi Rama Rao Namala <jagapathinamala@gmail.com>
2026-02-18 08:57:48 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (91.43%) - LeetSync

--- 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.cpp
+++ 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.cpp
@@ -3,7 +3,6 @@ public:
     bool hasAlternatingBits(int n) {
         long long x = n ^ (n >> 1);
         long long ans = x & (x + 1);
-        if( ans  == 0) return 1 ;
-        return 0 ;
+        return ans == 0 ;
     }
 };
