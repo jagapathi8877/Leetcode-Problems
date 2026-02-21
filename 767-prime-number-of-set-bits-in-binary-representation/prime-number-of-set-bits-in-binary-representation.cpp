a4fa3c15186dc16a6bfc316e6b10c815ab311f5a
Jagapathi Rama Rao Namala <jagapathinamala@gmail.com>
2026-02-21 09:58:31 +0530

    Time: 1161 ms (5.00%) | Memory: 7.9 MB (88.15%) - LeetSync

---
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int check(int n )
+    {
+        if(n <= 1 ) return 0 ;
+        if(n <= 3) return 1;
+        if(n % 2 == 0 || n%3 == 0) return 0 ;
+        for(int i = 5 ; i*i <= n ; i+=6) if(n%i == 0 || n % (i+2) == 0) return 0;
+        return 1; 
+    }
+    int countPrimeSetBits(int left, int right) {
+        int ans = 0 ;
+        for(int i = left ; i <= right ; i++)
+        {
+            int n = i ;
+            int cnt = 0 ;
+            while(n)
+            {
+                n &= (n-1);
+                cnt++;
+            }
+            cout << cnt << " " ;
+            if(check(cnt)) ans++;
+        }
+        cout << check(5) <<  " " ;
+        return ans ;
+    }
+};
