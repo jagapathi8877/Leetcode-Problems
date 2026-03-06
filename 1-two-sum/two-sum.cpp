1e8e9bf0e1a1694bfd00abab79555d786736dc25
Jagapathi Rama Rao Namala <jagapathinamala@gmail.com>
2026-03-06 15:57:10 +0530

    Time: 2 ms (73.03%) | Memory: 14.7 MB (52.09%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int>mp;
+        for(int i = 0 ; i < nums.size() ; i++)
+        {
+            int res = target - nums[i] ;
+            if(mp.count(res)) return {i , mp[res]} ;
+            else mp[nums[i]] = i ;;
+        }
+        return {-1,-1};
+    }
+};
